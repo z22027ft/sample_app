7e54e52735a61b640d854e74b8e0924c70daff5c
z22027ft <ubuntu@ip-172-31-11-56.ec2.internal>
2022-05-10 13:22:37 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+ # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
@@ -16,8 +23,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
-    # 現在のユーザーをログアウトする
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
